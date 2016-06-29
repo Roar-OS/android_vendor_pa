ab3cfad2974aa62eade2bb974f51268d7cb3ecba
nazmulhasan12 <nazmulhasanicaew@gmail.com>
2016-06-29 11:25:15 +0530

    fix bullhead
    Change-Id: I138147af3afe539edcfea518193ee15339a21d0a

--- products/bullhead/pa_bullhead.mk
+++ products/bullhead/pa_bullhead.mk
@@ -1,34 +1,20 @@
-# Copyright 2016 ParanoidAndroid Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-ifeq (pa_bullhead,$(TARGET_PRODUCT))
-
-# Get the defaults going.
 TARGET_BOOT_ANIMATION_RES := 1080
-include vendor/pa/main.mk
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/pa/main.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
-# Override AOSP defaults that we want changed.
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := pa_bullhead
-PRODUCT_DEVICE := bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
-PRODUCT_MANUFACTURER := LGE
+TARGET_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRODUCT_NAME=bullhead \
-	BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19V/2862947:user/release-keys \
-	PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19V 2862947 release-keys"
 
-endif
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MTC19V/2862947:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MTC19V 2862947 release-keys" \
+    BUILD_ID=MTC19V
